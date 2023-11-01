d46df320fe02944edcc96e451bc88bb60ca1ccca
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-01 04:27:54 -0400

    created logic for creating tablee

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,3 +1,10 @@
+DROP DATABASE IF EXISTS songs_dev;
+
+CREATE DATABASE songs_dev;
+
+\c songs_dev;
+
+
 CREATE TABLE songs (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
---
+++ back-end/db/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Song1', 'Tyga', 'Album1', '3:30', true),
+('Song2', 'Ashanti ', 'Album2', '4:15', false),
+('Song1', 'Whitney', 'Album1', '3:44', true),
+('Song2', 'Ne-yo', 'Album2', '2:39', false),
+('Song1', 'Beyonce', 'Album1', '3:30', false),
+('Song2', 'Monica ', 'Album2', '4:15', false),
+('Song1', 'Brittney', 'Album1', '3:37', true),
+('Song2', 'Micheal ', 'Album2', '3:15', true),
+('Song1', 'Tyga', 'Album1', '3:24', true),
+('Song2', 'Drake ', 'Album2', '2:37', false),
+('Song1', 'PopSmoke', 'Album1', '2:45', true),
+('Song2', 'Nelly', 'Album2', '3:15', false);
