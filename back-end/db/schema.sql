98e134498475b77c025436528f72a89d9b78764a
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-01 05:23:38 -0400

    successfully connected the table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,15 +1,4 @@
 DROP DATABASE IF EXISTS songs_dev;
+CREATE DATABASE songs_dev; \c songs_dev;
 
-CREATE DATABASE songs_dev;
-
-\c songs_dev;
-
-
-CREATE TABLE songs (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    artist TEXT NOT NULL,
-    album TEXT,
-    time TEXT,
-    is_favorite BOOLEAN
-);
+CREATE TABLE songs ( id SERIAL PRIMARY KEY, name TEXT NOT NULL, artist TEXT NOT NULL, album TEXT, time TEXT, is_favorite BOOLEAN );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,13 +1,11 @@
-INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES   
 ('Song1', 'Tyga', 'Album1', '3:30', true),
-('Song2', 'Ashanti ', 'Album2', '4:15', false),
-('Song1', 'Whitney', 'Album1', '3:44', true),
-('Song2', 'Ne-yo', 'Album2', '2:39', false),
-('Song1', 'Beyonce', 'Album1', '3:30', false),
-('Song2', 'Monica ', 'Album2', '4:15', false),
-('Song1', 'Brittney', 'Album1', '3:37', true),
-('Song2', 'Micheal ', 'Album2', '3:15', true),
-('Song1', 'Tyga', 'Album1', '3:24', true),
-('Song2', 'Drake ', 'Album2', '2:37', false),
-('Song1', 'PopSmoke', 'Album1', '2:45', true),
-('Song2', 'Nelly', 'Album2', '3:15', false);
+('Song2', 'Ashanti', 'Album2', '4:15', false),
+('Song3', 'Whitney', 'Album1', '3:44', true),
+('Song4', 'Ne-yo', 'Album2', '2:39', false),
+('Song5', 'Beyonce', 'Album1', '3:30', false),
+('Song6', 'Monica ', 'Album2', '4:15', false),
+('Song7', 'Brittney', 'Album1', '3:37', true),
+ ('Song8', 'Micheal ', 'Album2', '3:15', true),
+ ('Song9', 'Drake ', 'Album2', '2:37', false),
+ ('Song10', 'PopSmoke', 'Album1', '2:45', true);
